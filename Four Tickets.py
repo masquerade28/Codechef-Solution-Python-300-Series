d3b4e3e38f8de59957df2942b524043c34e38854
N.I.K <85172286+masquerade28@users.noreply.github.com>
2023-05-29 11:33:42 +0530

    Update Four Tickets.py

--- Four Tickets.py
+++ Four Tickets.py
@@ -4,10 +4,14 @@
   Determine whether they will be going to the concert or not.
 '''
 
+# Running Loop For Test Cases 
 for t in range(int(input())):
   
+  # Taking Input For X = Cost Of Each Ticket
   X = int(input())
   
+  # If Total Cost Of Tickets < 1000; They Will Go To The Concert
   FourTickets = "YES" if ((4*X) <= 1000) else "NO"
   
+  # Printing Result
   print(FourTickets)
