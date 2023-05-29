31e754289738dc4df748b8ec80f4157f7c7b5f97
N.I.K <85172286+masquerade28@users.noreply.github.com>
2023-05-29 11:56:53 +0530

    Update Mana Points.py

--- Mana Points.py
+++ Mana Points.py
@@ -5,8 +5,11 @@
   If Chefario currently has Y mana points, determine the maximum number of special attacks he can perform.
 '''
 
+# Running Loop For Test Cases
 for t in range(int(input())):
   
+  # Taking Input For X = No. Of Points Special Attack Costs; Y = Chef's Current Mana Points
   X,Y = map(int, input().split())
   
+  # Maximum Number Of Special Attacks He Can Perform = Y // X
   print(int(Y//X))
