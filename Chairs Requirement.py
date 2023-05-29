9df08126eb6cb9f31db5b08771327cb29fb66c3a
N.I.K <85172286+masquerade28@users.noreply.github.com>
2023-05-29 11:26:23 +0530

    Update Chairs Requirement.py

--- Chairs Requirement.py
+++ Chairs Requirement.py
@@ -5,10 +5,14 @@
   Determine the minimum number of new chairs Chef must buy so that every student is able to get one chair to sit on.
 '''
 
+# Running Loop For Test Cases
 for t in range(int(input())):
   
+  # Taking Input For X And Y, i.e; No. Of Students Joined And No. Of Chairs Chef Is Having
   X,Y = map(int, input().split())
   
+  # If No. Of Students > No. Of Chairs, Required No. Of Chairs = X - Y 
   Chairs = (X-Y) if X>Y else 0
   
+  # Printing Result
   print(Chairs)
