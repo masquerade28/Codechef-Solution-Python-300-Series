868e0dae81790d7c2baf0ef96aea6974f0699bb9
N.I.K <85172286+masquerade28@users.noreply.github.com>
2023-05-29 11:40:10 +0530

    Update Just One More Episode.py

--- Just One More Episode.py
+++ Just One More Episode.py
@@ -4,10 +4,14 @@
   If he starts watching a new episode now, will he finish watching it strictly before the exam starts?
 '''
 
+# Running Loop For Test Cases
 for t in range(int(input())):
   
+  # Taking Input For X = Time Chef Is Having
   X = int(input())
-    
+  
+  # If Time Chef Is Having > 24 Minutes; He Will Be Able To Finish It
   JustOneMoreEpisode = "YES" if X>24 else "NO"
-    
+  
+  # Displaying Result
   print(JustOneMoreEpisode)
