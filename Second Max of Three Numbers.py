1881404fa7124ed4084292822667d1e42af44ab5
N.I.K <85172286+masquerade28@users.noreply.github.com>
2023-05-29 12:40:43 +0530

    Update Second Max of Three Numbers.py

--- Second Max of Three Numbers.py
+++ Second Max of Three Numbers.py
@@ -2,24 +2,35 @@
   Write a program that accepts sets of three numbers, and prints the second-maximum number among the three.
 '''
 
+# Running Loop For Test Cases
 for t in range(int(input())):
   
-  x,y,z = map(int, input().split())
+  # Taking Input For X = First Number; Y = Second Number; Z = Third Number
+  X,Y,Z = map(int, input().split())
   
-  if x > y or x > z:
+  # If First Number > Second Number Or First Number > Third Number
+  if X > Y or X > Z:
     
-    if x < y or x < z:
+    # If First Number < Second Number Or First Number < Third Number
+    if X < Y or X < Z:
       
-      print(x)
-    
-  if y > x or y > z:
-        
-      if y < x or y < z:
-            
-          print(y)
+      # X Is The Second Maximum Number
+      print(X)
    
-  if z > x or z > y:
-        
-      if z < x or z < y:
-           
-          print(z)
+  # If Second Number > First Number Or Second Number > Third Number 
+  if Y > X or Y > Z:
+      
+      # If Second Number < First Number Or Second Number < Third Number
+      if Y < X or Y < Z:
+          
+          # Y Is The Second Maximum Number
+          print(Y)
+  
+  # If Third Number > First Number Or Third Number > Second Number
+  if Z > X or Z > Y:
+      
+      # If Third Number < First Number Or Third Number < Second Number
+      if Z < X or Z < Y:
+          
+          # Z Is The Second Maximum Number
+          print(Z)
