9101353d7e814693035a4a3d8eab65e7ba98def4
N.I.K <85172286+masquerade28@users.noreply.github.com>
2023-05-29 11:31:10 +0530

    Update Enough Space.py

--- Enough Space.py
+++ Enough Space.py
@@ -5,10 +5,14 @@
   Chef can save all the files on his computer only if the total size of the files is less than or equal to the space available on his computer.
 '''
 
+# Running Loop for Test Cases
 for t in range(int(input())):
-    
+  
+  # Taking Input For N = Free Space In Chef's Computer; X = Files Of 1 GB That Chef Want To Save; Y = Files Of 2 GB That Chef Want To Save
   N,X,Y = map(int, input().split())
     
+  # If Total Space Occupied By Files < Total Free Space Availabe; Then Chef Can Save All The Files
   EnoughSpace = "YES" if (X+(Y*2)) <= N else "NO"
-    
+   
+  # Printing Result
   print(EnoughSpace)
