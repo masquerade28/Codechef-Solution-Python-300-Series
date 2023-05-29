e36c9469bc7ac0e2c2408e1c028c04392f2f88f4
N.I.K <85172286+masquerade28@users.noreply.github.com>
2023-05-29 12:01:42 +0530

    Update Number Of Credits.py

--- Number Of Credits.py
+++ Number Of Credits.py
@@ -7,9 +7,11 @@
   Assuming that you cleared all your courses, report the number of credits you obtain this semester.
 '''
 
-
+# Running Loop For Test Cases
 for t in range(int(input())):
   
+  # Taking Input For X = No. Of RTP Courses; Y = No. Of Audit Courses; Z = No. Of Non-RTP Courses
   X,Y,Z = map(int, input().split())
-    
+  
+  # No. Of Credits = RTP's Credit * No. Of RTP Courses + Audit's Credit * No. Of Audit Courses + Non-Audit's Credit * No. Of Non-Audit Courses
   print((X*4)+(Y*2))
