52a3cd5a1ae56aed2822393955d5e4591cb4aafd
N.I.K <85172286+masquerade28@users.noreply.github.com>
2023-05-29 12:08:26 +0530

    Update Tasty Decisions.py

--- Tasty Decisions.py
+++ Tasty Decisions.py
@@ -8,15 +8,20 @@
   Print Chocolate if the packet of chocolate is tastier, Candy if the packet of candy is tastier, and Either if they have the same tastiness.
 '''
 
+# Running Loop For Test Cases
 for t in range(int(input())):
   
+  # Taking Input For X = Tastiness Of Chocolate; Y = Tastiness Of Candy
   X,Y = map(int, input().split())
-    
+  
+  # Checking If Total Tastiness Of Chocolate Is Greater Than Total Tastiness Of Candy
   if (X*2) > (Y*5):
       print("Chocolate")
-      
+  
+  # Checking If Total Tastiness Of Chocolate Is Equal To Total Tastiness Of Candy
   elif (X*2) == (Y*5):
       print("Either")
-    
+ 
+  # Else Candy Will Be More Tastier
   else:
       print("Candy")
