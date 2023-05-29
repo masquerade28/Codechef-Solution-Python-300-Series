cef61eff33c1780a50a4eefee61541c6765e3d2a
N.I.K <85172286+masquerade28@users.noreply.github.com>
2023-05-29 12:12:21 +0530

    Update Waiting Time.py

--- Waiting Time.py
+++ Waiting Time.py
@@ -6,8 +6,11 @@
   It is guaranteed that the information has not been revealed to the Chef yet.
 '''
 
+# Running Loop For Test Cases
 for t in range(int(input())):
   
+  # Taking Input For K = No. Of Weeks After Which The Information Will Be Revealed; X = No. Of Days Passed
   K,X = map(int, input().split())
-    
+  
+  # No. Of Remaining Days = (7*K)-X
   print((7*K)-X)
