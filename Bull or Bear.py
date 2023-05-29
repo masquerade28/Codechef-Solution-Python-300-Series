6277d5366711e49a522b32b495aae682bc836603
N.I.K <85172286+masquerade28@users.noreply.github.com>
2023-05-29 11:20:17 +0530

    Update Bull or Bear.py

--- Bull or Bear.py
+++ Bull or Bear.py
@@ -4,15 +4,20 @@
   Help him calculate whether he made a profit, loss, or was it a neutral deal.
 '''
 
+# Running Loop For Test Cases
 for t in range(int(input())):
   
+  # Taking Input For X And Y ,i.e; Price At Which Stock Was Bought And Price At Which It Was Sold
   X,Y = map(int, input().split())
   
+  # If Bought Price > Selling Price, Chef Is In Loss
   if X > Y:
       print("LOSS")
-      
+  
+  # If Bought Price = Selling Price, Chef Is Neither In Loss Nor In Profit
   elif X == Y:
       print("NEUTRAL")
-      
+  
+  # If Bought Price < Selling Price, Chef Is In Profit
   else:
       print("PROFIT")
